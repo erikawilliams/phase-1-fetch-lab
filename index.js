1719a3f981616e779954df80b1abd7a3a59ec333
Erika Williams <erika@erikawilliams.us>
2022-03-22 08:44:08 -0600

    commit tree b9fa03f4a6342462659218e7b25f7cd87d389c9f

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+let GOTBooks;
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  console.log('fetchBooks running')
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp)=>resp.json())
+  .then((json)=> {
+    GOTBooks = json
+    renderBooks(GOTBooks)
+    console.log(GOTBooks)
+  })
 }
 
+
 function renderBooks(books) {
+  console.log('renderBooks running')
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -13,5 +24,8 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  console.log('domcontent loaded')
   fetchBooks();
 });
+
+
